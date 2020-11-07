a91003495fb41b306d9c216c6086616e2461bbac
yael bilu <yael@vitrainer.com>
2020-11-07 16:57:00 +0200

    first commit

--- culturetrip/Views/RootViewController.swift
+++ culturetrip/Views/RootViewController.swift
@@ -9,6 +9,20 @@ import UIKit
 
 class RootViewController: UIViewController {
     //  MARK: outlets
+    @IBOutlet weak var startButton: UIButton! {
+        didSet {
+            startButton.layer.cornerRadius = 8
+            startButton.layer.shadowOpacity = 0.8
+            startButton.layer.shadowOffset = CGSize(width: 1, height: 1)
+            startButton.layer.borderWidth = 2
+            startButton.layer.borderColor = UIColor.darkGray.cgColor
+        }
+    }
+    
+    override func viewDidLoad() {
+        title = "culturetrip"
+    }
+    
     @IBAction func startTapped() {
         DataProvider.shared.fetchData { result in
             switch result {
